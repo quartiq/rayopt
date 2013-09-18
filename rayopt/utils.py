39198d651cf541cc89adf4122bf9d101e12fa0ce
Robert Jordens <jordens@phys.ethz.ch>
2013-09-17 19:37:53 -0600

    add missing utils

---
+++ rayopt/utils.py
@@ -0,0 +1,32 @@
+# -*- coding: utf8 -*-
+#
+#   pyrayopt - raytracing for optical imaging systems
+#   Copyright (C) 2013 Robert Jordens <jordens@phys.ethz.ch>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+
+
+def dir_to_angles(r):
+    return r/np.linalg.norm(r)
+
+def tanarcsin(u):
+    return u/np.sqrt(1 - np.square(u))
+
+def sinarctan(u):
+    return u/np.sqrt(1 + np.square(u))
+
+
+
