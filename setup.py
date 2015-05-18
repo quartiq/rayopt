192833954d08812a693dfd61e72ce8e445f5c7d3
Robert Jordens <jordens@gmail.com>
2015-05-18 14:23:02 -0600

    cleanup setup, permissions

--- setup.py
+++ setup.py
@@ -21,47 +21,39 @@ from __future__ import print_function, absolute_import, division
 
 from setuptools import setup, find_packages, Extension
 from Cython.Distutils import build_ext
-import numpy
 
 setup(
         name = "pyrayopt",
         version = "0.0+dev",
-        description = "raytracing for optical imaging systems",
-        long_description = """build on enthought/{traits, chaco}.""",
+        description = "raytracing and lens design framework",
+        long_description = open("README.rst").read(),
         author = "Robert Jordens",
-        author_email = "jordens@phys.ethz.ch",
-        url = "http://launchpad.net/pyrayopt",
+        author_email = "jordens@gmail.com",
+        url = "http://github.com/jordens/pyrayopt",
         license = "GPLv3+",
         keywords = "optics lens raytracing optimization point spread",
         install_requires = [
-            "numpy", "scipy", "matplotlib", "nose", "pyyaml",
-            "sqlalchemy"],
-        extras_require = {
-            "gui": ["chaco", "traitsui"],
-            },
+            "numpy", "scipy", "matplotlib", "pyyaml", "sqlalchemy", "cython",
+        ],
+        extras_require = {},
         #dependency_links = [],
         packages = find_packages(),
         #namespace_packages = [],
-        test_suite = "nose.collector",
+        test_suite = "rayopt.tests",
         ext_modules=[
                 Extension("rayopt._transformations",
                     sources=["rayopt/_transformations.c"],),
             ],
         cmdclass = {"build_ext": build_ext},
-
-        entry_points = {
-            #"gui_scripts": ["bullseye = bullseye.app:main"],
-            },
+        entry_points = {},
         include_package_data = True,
         classifiers = [f.strip() for f in """
             Development Status :: 4 - Beta
-            Environment :: X11 Applications :: GTK
-            Environment :: X11 Applications :: Qt
             Intended Audience :: Science/Research
             License :: OSI Approved :: GNU General Public License (GPL)
             Operating System :: OS Independent
             Programming Language :: Python :: 2
-            Topic :: Multimedia :: Graphics :: Capture :: Digital Camera
+            Programming Language :: Python :: 3
             Topic :: Scientific/Engineering :: Physics
         """.splitlines() if f.strip()],
-        )
+)
