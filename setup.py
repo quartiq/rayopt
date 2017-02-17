07295a85975571081962ba1cbbbe724c3a4959f9
Robert Jordens <jordens@gmail.com>
2017-02-17 15:56:19 +0100

    relicense as LGPLv3+

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@
 #   Copyright (C) 2012 Robert Jordens <jordens@phys.ethz.ch>
 #
 #   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
+#   it under the terms of the GNU Lesser General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+#   GNU Lesser General Public License for more details.
 #
-#   You should have received a copy of the GNU General Public License
+#   You should have received a copy of the GNU Lesser General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import, print_function, division
@@ -34,7 +34,7 @@ setup(
     author="Robert Jordens",
     author_email="jordens@gmail.com",
     url="https://github.com/jordens/rayopt",
-    license="GPLv3+",
+    license="LGPLv3+",
     keywords=("optics lens design raytracing optimization "
               "point spread opd aberration"),
     install_requires=[
@@ -63,7 +63,7 @@ setup(
     classifiers=[f.strip() for f in """
         Development Status :: 4 - Beta
         Intended Audience :: Science/Research
-        License :: OSI Approved :: GNU General Public License (GPL)
+        License :: OSI Approved :: GNU Lesser General Public License (LGPL)
         Operating System :: OS Independent
         Programming Language :: Python :: 2
         Programming Language :: Python :: 3
